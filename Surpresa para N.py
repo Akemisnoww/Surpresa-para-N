2f148bc0eaf8226cc357417b9153e2df994f47fe
Akemi 3 <akemisnoww@gmail.com>
2024-10-05 12:15:00 -0300

    Add files via upload

---
+++ Surpresa para N.py
@@ -0,0 +1,18 @@
+import time
+
+def aniversario():
+    
+    print("Feliz aniversário Nick, eu te Amooo")
+    time.sleep(2)  
+    
+    resposta = input("Mas e agora? O quanto você me ama? ")
+
+
+    if resposta == "Mais que tudo nesse mundo <3, obrigada amor":
+        
+        print("\n  ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥")
+        print("     ♥          ♥")
+        print("        ♥   ♥")
+        print("           ♥")
+
+aniversario()
